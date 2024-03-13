1e682fd9d3d091fc7280e5ccb02651f143347540
takayuki.wada <yakato.jun@gmail.com>
2024-03-13 09:28:43 +0900

    mysql2の記法にCDatabase.jsを書き換え

--- backend/app/modules/CDatabase.js
+++ backend/app/modules/CDatabase.js
@@ -2,30 +2,30 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var http = require("http");
 var fs = require("fs");
-var mysql = require("mysql");
+const mysql = require("mysql2/promise");
 var CDatabase = /** @class */ (function () {
     function CDatabase() {
     }
     CDatabase.prototype.createDatabaseTables = function () {
         // DBに接続する設定情報
-        var con = mysql.createConnection({
+        mysql.createConnection({
             host: "localhost",
             user: "root",
             password: "tagamiroot",
             database: "tagami_db",
-        });
-        con.connect(function (err) {
-            if (err)
-                throw err;
-            console.log("接続完了");
-            con.query("CREATE TABLE person (name VARCHAR(10) NOT NULL, age int NOT NULL)", function (err, result) {
-                if (err)
-                    throw err;
+        })
+            .then(function (con) {
+                console.log("接続完了");
+                return con.query("CREATE TABLE person (name VARCHAR(10) NOT NULL, age int NOT NULL)");
+            })
+            .then(function (result) {
                 console.log("テーブルが作成されました");
                 console.log(result);
+            })
+            .catch(function (err) {
+                console.log(err);
             });
-            console.log(err);
-        });
+
     };
     return CDatabase;
 }());
